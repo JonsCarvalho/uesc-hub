96f2eadb90bdfbc1be94f2c259af06c329977b4c
Jônatas Moreira de Carvalho <jonatas_carvalhow@live.com>
2020-03-25 19:04:03 -0300

    create local storage

--- lib/app/app_module.dart
+++ lib/app/app_module.dart
@@ -3,11 +3,21 @@ import 'package:flutter_modular/flutter_modular.dart';
 import 'package:flutter/material.dart';
 import 'package:uesc_hub/app/app_widget.dart';
 import 'package:uesc_hub/app/modules/home/home_module.dart';
+import 'package:uesc_hub/app/shared/auth/auth_controller.dart';
+import 'package:uesc_hub/app/shared/auth/repositories/auth_repository.dart';
+import 'package:uesc_hub/app/shared/auth/repositories/auth_repository_interface.dart';
+import 'package:uesc_hub/app/shared/repositories/local_storage/local_storage_hive.dart';
+import 'package:uesc_hub/app/shared/repositories/local_storage/local_storage_interface.dart';
+import 'package:uesc_hub/app/shared/repositories/local_storage/local_storage_shared.dart';
 
 class AppModule extends MainModule {
   @override
   List<Bind> get binds => [
         Bind((i) => AppController()),
+        // Bind<ILocalStorage>((i) => LocalStorageHive()),
+        Bind<ILocalStorage>((i) => LocalStorageShared()),
+        Bind<IAuthRepository>((i) => AuthRepository()),
+        Bind((i) => AuthController()),
       ];
 
   @override

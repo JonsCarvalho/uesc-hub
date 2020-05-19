058aad0685f565c36efdddccc159d40f74d75bec
JonsCarvalho <jonatas_carvalhow@live.com>
2020-05-19 07:12:08 -0300

    added google maps

--- lib/app/navigation/navigation_module.dart
+++ lib/app/navigation/navigation_module.dart
@@ -1,3 +1,4 @@
+import 'package:google_maps_flutter/google_maps_flutter.dart';
 import 'package:uesc_hub/app/navigation/navigation_controller.dart';
 import 'package:flutter_modular/flutter_modular.dart';
 import 'package:uesc_hub/app/navigation/navigation_page.dart';
@@ -6,6 +7,14 @@ class NavigationModule extends ChildModule {
   @override
   List<Bind> get binds => [
         Bind((i) => NavigationController()),
+        // Bind(
+        //   (i) => GoogleMap(
+        //     initialCameraPosition: CameraPosition(
+        //       target: LatLng(37.42796133580664, -122.085749655962),
+        //       zoom: 14.4746,
+        //     ),
+        //   ),
+        // ),
       ];
 
   @override
--- lib/app/navigation/navigation_page.dart
+++ lib/app/navigation/navigation_page.dart
@@ -1,5 +1,8 @@
+import 'dart:async';
+
 import 'package:flutter/material.dart';
 import 'package:flutter_modular/flutter_modular.dart';
+import 'package:google_maps_flutter/google_maps_flutter.dart';
 import 'navigation_controller.dart';
 
 class NavigationPage extends StatefulWidget {
@@ -12,7 +15,18 @@ class NavigationPage extends StatefulWidget {
 
 class _NavigationPageState
     extends ModularState<NavigationPage, NavigationController> {
-  //use 'controller' variable to access controller
+  Completer<GoogleMapController> _controller = Completer();
+
+  final CameraPosition _kGooglePlex = CameraPosition(
+    target: LatLng(37.42796133580664, -122.085749655962),
+    zoom: 14.4746,
+  );
+
+  final CameraPosition _kLake = CameraPosition(
+      bearing: 192.8334901395799,
+      target: LatLng(37.43296265331129, -122.08832357078792),
+      tilt: 59.440717697143555,
+      zoom: 19.151926040649414);
 
   @override
   Widget build(BuildContext context) {
@@ -20,9 +34,34 @@ class _NavigationPageState
       appBar: AppBar(
         title: Text(widget.title),
       ),
-      body: Column(
-        children: <Widget>[],
+      body: Container(
+        width: double.infinity,
+        height: double.infinity,
+        child: Stack(
+          children: [
+            GoogleMap(
+              zoomControlsEnabled: false,
+              myLocationButtonEnabled: true,
+              myLocationEnabled: true,
+              buildingsEnabled: true,
+              initialCameraPosition: _kGooglePlex,
+              onMapCreated: (GoogleMapController controller) {
+                _controller.complete(controller);
+              },
+            ),
+          ],
+        ),
+      ),
+      floatingActionButton: FloatingActionButton.extended(
+        onPressed: _goToTheLake,
+        label: Text('To the lake!'),
+        icon: Icon(Icons.directions_boat),
       ),
     );
+  }
+
+  Future<void> _goToTheLake() async {
+    final GoogleMapController controller = await _controller.future;
+    controller.animateCamera(CameraUpdate.newCameraPosition(_kLake));
   }
 }

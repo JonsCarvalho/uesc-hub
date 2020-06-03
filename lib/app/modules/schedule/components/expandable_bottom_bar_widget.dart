5b6f80e4d908e21ddf4f52589afdb29cb5f58b27
JonsCarvalho <jonatas_carvalhow@live.com>
2020-06-03 08:05:38 -0300

    updated decorations

--- lib/app/modules/schedule/components/expandable_bottom_bar_widget.dart
+++ lib/app/modules/schedule/components/expandable_bottom_bar_widget.dart
@@ -33,7 +33,11 @@ class ExpandableBottomBar extends StatelessWidget {
           shape: AutomaticNotchedShape(
             RoundedRectangleBorder(),
             StadiumBorder(
-              side: BorderSide(),
+              side: BorderSide(
+                style: BorderStyle.solid,
+                color: Colors.black,
+                width: 2,
+              ),
             ),
           ),
           bottomAppBarColor: Theme.of(_).appBarTheme.color,
@@ -54,15 +58,15 @@ class ExpandableBottomBar extends StatelessWidget {
           ),
           expandedDecoration: BoxDecoration(
             borderRadius: BorderRadius.circular(30.0),
-            color: ThemeData.dark().backgroundColor,
+            color: Theme.of(_).bottomAppBarColor,
             boxShadow: [
               BoxShadow(
-                color: Colors.black.withAlpha(100),
+                color: Colors.grey.withAlpha(100),
                 blurRadius: 6.0,
                 spreadRadius: 0.0,
                 offset: Offset(
-                  4.0,
                   0.0,
+                  -3.0,
                 ),
               ),
             ],
@@ -80,118 +84,139 @@ class ExpandableBottomBar extends StatelessWidget {
               ],
             ),
           ),
-          bottomAppBarBody: Row(
-            mainAxisSize: MainAxisSize.max,
-            children: <Widget>[
-              Expanded(
-                child: Stack(
-                  children: [
-                    Align(
-                      alignment: Alignment.topLeft,
-                      child: AnimatedContainer(
-                        duration: Duration(milliseconds: 400),
-                        curve: Curves.easeInCubic,
-                        decoration: BoxDecoration(
-                          color: scheduleController.bottomBarMenuSelected == 0
-                              ? Theme.of(_).primaryColor
-                              : Colors.transparent,
-                          boxShadow: [
-                            BoxShadow(
-                              color:
-                                  Theme.of(context).primaryColor.withAlpha(100),
-                              blurRadius: 6.0,
-                              spreadRadius: 0.5,
-                              offset: Offset(
-                                0.0,
-                                3.0,
-                              ),
-                            )
-                          ],
+          bottomAppBarBody: Container(
+            decoration: BoxDecoration(
+              border: Border(
+                top: BorderSide(
+                  color: Theme.of(_).primaryColor.withAlpha(50),
+                  style: BorderStyle.solid,
+                  width: 1,
+                ),
+              ),
+              gradient: LinearGradient(
+                colors: [
+                  // Colors.grey.withAlpha(100),
+                  Colors.blue.withAlpha(50),
+                  Theme.of(_).primaryColor.withAlpha(50),
+                  Colors.green.withAlpha(50),
+                ],
+              ),
+            ),
+            child: Row(
+              mainAxisSize: MainAxisSize.max,
+              children: <Widget>[
+                Expanded(
+                  child: Stack(
+                    children: [
+                      Align(
+                        alignment: Alignment.topLeft,
+                        child: AnimatedContainer(
+                          duration: Duration(milliseconds: 400),
+                          curve: Curves.easeInCubic,
+                          decoration: BoxDecoration(
+                            color: scheduleController.bottomBarMenuSelected == 0
+                                ? Theme.of(_).primaryColor
+                                : Colors.transparent,
+                            boxShadow: [
+                              BoxShadow(
+                                color: Theme.of(context)
+                                    .primaryColor
+                                    .withAlpha(100),
+                                blurRadius: 6.0,
+                                spreadRadius: 0.0,
+                                offset: Offset(
+                                  0.0,
+                                  3.0,
+                                ),
+                              )
+                            ],
+                          ),
+                          height: 7,
+                          width: scheduleController.bottomBarMenuSelected == 0
+                              ? MediaQuery.of(context).size.width / 2
+                              : .1,
                         ),
-                        height: 5,
-                        width: scheduleController.bottomBarMenuSelected == 0
-                            ? MediaQuery.of(context).size.width / 2
-                            : .1,
                       ),
-                    ),
-                    FlatButton(
-                      onPressed: () {
-                        scheduleController.changeBottomBarMenuSelected(0);
-                      },
-                      child: Align(
-                        alignment: Alignment.centerLeft,
+                      FlatButton(
+                        onPressed: () {
+                          scheduleController.changeBottomBarMenuSelected(0);
+                        },
                         child: Align(
                           alignment: Alignment.centerLeft,
-                          child: Padding(
-                            padding: const EdgeInsets.only(left: 8.0),
-                            child: Text(
-                              "Tarefas",
-                              style: TextStyle(
-                                fontSize: 14,
+                          child: Align(
+                            alignment: Alignment.centerLeft,
+                            child: Padding(
+                              padding: const EdgeInsets.only(left: 8.0),
+                              child: Text(
+                                "Tarefas",
+                                style: TextStyle(
+                                  fontSize: 14,
+                                ),
                               ),
                             ),
                           ),
                         ),
                       ),
-                    ),
-                  ],
+                    ],
+                  ),
                 ),
-              ),
-              Expanded(
-                child: Stack(
-                  children: [
-                    Align(
-                      alignment: Alignment.topRight,
-                      child: AnimatedContainer(
-                        duration: Duration(milliseconds: 400),
-                        curve: Curves.easeInCubic,
-                        decoration: BoxDecoration(
-                          color: scheduleController.bottomBarMenuSelected == 1
-                              ? Theme.of(_).primaryColor
-                              : Colors.transparent,
-                          boxShadow: [
-                            BoxShadow(
-                              color:
-                                  Theme.of(context).primaryColor.withAlpha(100),
-                              blurRadius: 6.0,
-                              spreadRadius: 0.5,
-                              offset: Offset(
-                                0.0,
-                                3.0,
-                              ),
-                            )
-                          ],
+                Expanded(
+                  child: Stack(
+                    children: [
+                      Align(
+                        alignment: Alignment.topRight,
+                        child: AnimatedContainer(
+                          duration: Duration(milliseconds: 400),
+                          curve: Curves.easeInCubic,
+                          decoration: BoxDecoration(
+                            color: scheduleController.bottomBarMenuSelected == 1
+                                ? Theme.of(_).primaryColor
+                                : Colors.transparent,
+                            boxShadow: [
+                              BoxShadow(
+                                color: Theme.of(context)
+                                    .primaryColor
+                                    .withAlpha(100),
+                                blurRadius: 6.0,
+                                spreadRadius: 0.0,
+                                offset: Offset(
+                                  0.0,
+                                  3.0,
+                                ),
+                              )
+                            ],
+                          ),
+                          height: 7,
+                          width: scheduleController.bottomBarMenuSelected == 1
+                              ? MediaQuery.of(context).size.width / 2
+                              : .1,
                         ),
-                        height: 5,
-                        width: scheduleController.bottomBarMenuSelected == 1
-                            ? MediaQuery.of(context).size.width / 2
-                            : .1,
                       ),
-                    ),
-                    FlatButton(
-                      onPressed: () {
-                        scheduleController.changeBottomBarMenuSelected(1);
-                      },
-                      child: Align(
-                        alignment: Alignment.centerRight,
+                      FlatButton(
+                        onPressed: () {
+                          scheduleController.changeBottomBarMenuSelected(1);
+                        },
                         child: Align(
                           alignment: Alignment.centerRight,
-                          child: Padding(
-                            padding: const EdgeInsets.only(right: 8.0),
-                            child: Text(
-                              "Provas",
-                              style: TextStyle(
-                                fontSize: 14,
+                          child: Align(
+                            alignment: Alignment.centerRight,
+                            child: Padding(
+                              padding: const EdgeInsets.only(right: 8.0),
+                              child: Text(
+                                "Provas",
+                                style: TextStyle(
+                                  fontSize: 14,
+                                ),
                               ),
                             ),
                           ),
                         ),
                       ),
-                    ),
-                  ],
+                    ],
+                  ),
                 ),
-              ),
-            ],
+              ],
+            ),
           ),
         );
       },

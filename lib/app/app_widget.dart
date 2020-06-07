1bbcf7e4c477923833853e48ec925829d47ae963
JonsCarvalho <jonatas_carvalhow@live.com>
2020-06-07 02:48:46 -0300

    created light theme

--- lib/app/app_widget.dart
+++ lib/app/app_widget.dart
@@ -16,9 +16,20 @@ class AppWidget extends StatelessWidget {
       supportedLocales: [const Locale('pt', 'BR')],
       title: 'UESC Hub',
       theme: ThemeData(
-        primarySwatch: Colors.teal,
-        fontFamily: "ProductSans"
-      ),
+          primarySwatch: Colors.cyan,
+          primaryColor: Colors.cyan,
+          primaryColorDark: Color.fromRGBO(0, 149, 164, 1),
+          primaryColorLight: Color.fromRGBO(178, 235, 242, 1),
+          accentColor: Color.fromRGBO(255, 87, 34, 1),
+          iconTheme: IconThemeData(color: Colors.black),
+          textTheme: TextTheme(
+            button: TextStyle().copyWith(color: Colors.white),
+            headline1: TextStyle().copyWith(color: Color.fromRGBO(33, 33, 33,1)),
+            headline2: TextStyle().copyWith(color: Color.fromRGBO(117, 117, 117,1)),
+            
+          ),
+          dividerColor: Color.fromRGBO(189, 189, 189, 1),
+          fontFamily: "ProductSans"),
       initialRoute: '/',
       onGenerateRoute: Modular.generateRoute,
     );
